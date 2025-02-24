7ec14ca8b3a15e14e6373b9837c7b84411fb60f2
briasari660 <briasari660@SDS-L-F8QLG63>
2025-02-24 14:00:35 -0500

    add button

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +14,7 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // Global Variabales go here
-
+        List<Player> players = new List<Player>();
 
         public Form1()
         {
@@ -22,7 +23,21 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            string name = nameInput.Text;
+            string age = ageInput.Text;
+            string team = teamInput.Text;
+            string position = positionInput.Text;
 
+            Player newPlayer = new Player(name, age, team, position);
+            players.Add(newPlayer);
+
+            nameInput.Text = ageInput.Text = teamInput.Text = positionInput.Text = "";
+            nameInput.Focus();
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                outputLabel.Text += $"{players[i].name}, {players[i].age}, {players[i].team}, {players[i].position}\n";
+            }
         }
 
         private void removeButton_Click(object sender, EventArgs e)
--- ClassInventory/Player.cs
+++ ClassInventory/Player.cs
@@ -8,5 +8,18 @@ namespace ClassInventory
 {
     internal class Player
     {
+        //class attributes
+        public string name, age, team, position;
+
+        //constructor method
+        public Player(string _name, string _age, string _team, string _position)
+        {
+            name = _name;
+            age = _age;
+            team = _team;
+            position = _position;
+
+        }
+
     }
 }
